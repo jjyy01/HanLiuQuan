dc8231b0c6beb04c10f6c6362d4ad502cf0b3628
jiwei <jiweiq@qq.com>
2015-01-03 18:14:15 +0800

    Initial Commit

---
+++ HanLiuQuan/QYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QYAppDelegate.h
+//  HanLiuQuan
+//
+//  Created by jiwei on 15/1/3.
+//  Copyright (c) 2015年 weiji.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
